7c74834cac179a79413872fad5592e8ef13515d5
Chao Ruan <chaoruan818@gmail.com>
2014-05-08 23:11:53 +1000

    .next implement

--- index.js
+++ index.js
@@ -1,14 +1,55 @@
 var http = require('http');
 
 module.exports = function(){
-  var express =  function (request,response){
-    response.statusCode = 404;
-    response.end();
+  function express(req,res,next){
+    express.handle(req, res, next);
   }
 
   express.listen = function(port,done){
     return  http.createServer(this).listen(port,done);
   }
 
+  express.stack = [];
+  express.use = function(callback){
+    this.stack.push(callback);
+  }
+  express.handle = function(req,res,out){
+    var index = 0;
+    var stack = this.stack;
+    function next(error){
+      var layer = stack[index++];
+      if(!layer)
+        {
+          if (out) return out(error);
+          if (error) {
+            res.statusCode = 500;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('500 - Internal Error');
+          } else {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end("404 - Not Found");
+            return;
+          }
+        }
+        try {
+          var arity = layer.length;
+          if (error) {
+            if (arity == 4) {
+              layer(error, req, res, next);
+            } else {
+              next(error);
+            }
+          } else if (arity < 4) {
+            layer(req, res, next);
+          } else {
+            next();
+          }
+        } catch (e) {
+          next(e);
+        }
+    }
+    next();
+  }
   return express;
 }
--- test/app_spec.js
+++ test/app_spec.js
@@ -33,3 +33,57 @@ describe("app",function() {
     });
   });
 });
+
+describe(".use",function() {
+  var app;
+  var m1 = function(){};
+  var m2 = function(){};
+  before(function(){
+    app = express();
+  });
+  it('should able to add stacks',function(){
+    app.use(m1);
+    app.use(m2);
+    expect(app.stack.length).to.eql(2);
+  });
+});
+
+describe("calling middleware stack",function() {
+  var app;
+  beforeEach(function() {
+    app = new express();
+  });
+  it('should be able to call a single middleware',function(done){
+    var m1 = function(req,res,next) {
+      res.end("hello from m1");
+    };
+    app.use(m1);
+    request(app).get('/').expect('hello from m1').end(done)
+  });
+
+  it('Should be able to call next to go to the next middleware',function(done){
+    var m1 = function(req,res,next) {
+      next();
+    };
+
+    var m2 = function(req,res,next) {
+      res.end("hello from m2");
+    };
+    app.use(m1);
+    app.use(m2);
+    request(app).get('/').expect('hello from m2').end(done);
+  });
+
+  it('Should 404 at the end of middleware chain',function(done){
+    var m1 = function(req,res,next) {
+      next();
+    };
+
+    var m2 = function(req,res,next) {
+      next();
+    };
+    app.use(m1);
+    app.use(m2);
+    request(app).get('/').expect(404).end(done);
+  });
+});
